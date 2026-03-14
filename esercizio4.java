744357fbb9cc1712abdcb538ad12367b42beb333
Noccio21 <nicho241008@gmail.com>
2026-03-14 12:46:53 +0100

    nicholas

---
+++ esercizio4.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static int somma(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int risultato = somma(5, 3);
+        System.out.println("La somma è: " + risultato);
+    }
+}
