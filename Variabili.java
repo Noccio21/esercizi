7f844a6d9b22774759ddadf1b5541eb733d00843
Noccio21 <nicho241008@gmail.com>
2026-03-14 12:35:09 +0100

    noccio

---
+++ Variabili.java
@@ -0,0 +1,12 @@
+public class Variabili {
+    public static void main(String[] args) {
+
+        int numero = 10;
+        double prezzo = 19.99;
+        String messaggio = "Ciao mondo";
+
+        System.out.println("Valore int: " + numero);
+        System.out.println("Valore double: " + prezzo);
+        System.out.println("Valore String: " + messaggio);
+    }
+}
